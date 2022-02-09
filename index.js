54197734e0b6a64a1725990adad7b2e37d258795
Hilful <hilful25@gmail.com>
2022-02-10 00:34:43 +0600

    Manipulating Styles with jQuery

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 // alert("Working!!");
 
-$("h1").css("color", "red");
+// $("h1").css("color", "red");
+
+console.log($("h1").css("color"));
+console.log($("h1").css("font-size"));
+
+$("h1").addClass("big-title margin-50");
+// $("h1").removeClass("big-title");
+console.log($("h1").hasClass("margin-50"));
